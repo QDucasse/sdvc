68da229fb0e0033307e2250ba98e0a6dc6d03264
Quentin DUCASSE <quentin.ducasse@ensta-bretagne.org>
2021-02-15 17:31:39 +0100

    First attempt at a CLI

--- src/main.c
+++ src/main.c
@@ -1,13 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <argp.h>
 
 #include "compiler.h"
 #include "scanner.h"
 
 /* ==================================
+      COMMAND LINE ARGUMENTS
+====================================*/
+
+/* Communication between main and parser_opt */
+struct arguments {
+  int verbose;              /* Verbose mode */
+  char *outfile;            /* Argument for -o */
+  char *stringCompile;      /* Argument for -c */
+  char *stringScan;         /* Argument for -s */
+  char *stringDisassemble;  /* Argument for -d */
+};
+
+/* CLI options (name, key, arg, flags, doc) */
+static struct argp_option options[] = {
+  {"verbose", 'v', 0, 0, "Produce verbose output (register states after each compile step)."},
+  {"compile", 'c', "INFILE", 0, "Compile INFILE into a succession of instructions."},
+  {"scan", 's', "INFILE", 0, "Scan INFILE into a succession of tokens."},
+  {"disassemble", 'd', "INFILE", 0, "Disassemble INFILE into a succession of instructions."},
+  {"output", 'o', "OUTFILE", 0, "Output to OUTFILE (instead of standard output)."}
+};
+
+/* Email reporting */
+const char *argp_program_bug_address = "quentin.ducasse@ensta-bretagne.org";
+
+/* Mandatory arguments */
+static char args_doc[] = "All arguments need a flag.";
+
+/* Order of parameters: KEY, ARG, STATE. */
+static error_t parse_opt (int key, char *arg, struct argp_state *state) {
+  struct arguments *arguments = state->input;
+  switch (key) {
+    case 'v': arguments->verbose = 1;             break;
+    case 'c': arguments->stringCompile = arg;     break;
+    case 's': arguments->stringScan = arg;        break;
+    case 'd': arguments->stringDisassemble = arg; break;
+    case 'o': arguments->outfile = arg;           break;
+    case ARGP_KEY_ARG:
+      if (state->arg_num >= 2) argp_usage(state);
+      break;
+    case ARGP_KEY_END:
+      if (state->arg_num < 2) argp_usage (state);
+      break;
+    default: return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
+}
+
+/* Documentation of the command */
+static char doc[] = "sdvc -- SDVE compiler and disassembler.";
+
+/* The ARGP structure itself */
+static struct argp argp = {options, parse_opt, args_doc, doc};
+
+
+/* ==================================
           EXECUTION METHODS
 ====================================*/
+
+/* File handling
+============= */
 
 /* Read a file and return the contents in a buffer */
 static char* readFile(const char* path) {
@@ -44,8 +103,16 @@ static char* readFile(const char* path) {
   return buffer;
 }
 
+/* Write a file */
+static void writeFile(const char* source, FILE* dest) {
+
+}
+
+/* Actions
+======= */
+
 /* Scan a given file */
-static void scanFile(const char* path) {
+static void scanFile(const char* path, FILE* outstream) {
   char* source = readFile(path);
   initScanner(source);
   Token token;
@@ -53,12 +120,27 @@ static void scanFile(const char* path) {
     token = scanToken();
     printToken(token);
     if (token.type == TOKEN_EOF) {
-      printf("%s: ", path);
-      printf("File scanned\n");
+      fprintf(outstream, "File scanned\n");
       break;
     }
-
   }
+  free(source);
+}
+
+/* Compiler a given file */
+static void compileFile(const char* path, FILE* outstream) {
+  initCompiler();
+  char* source = readFile(path);
+  compile(source);
+  writeFile(path, outstream);
+  freeCompiler();
+  free(source);
+}
+
+/* Disassemble a given file */
+static void disassembleFile(const char* path, FILE* outstream) {
+  char* source = readFile(path);
+  writeFile(source, outstream);
   free(source);
 }
 
@@ -66,16 +148,48 @@ static void scanFile(const char* path) {
                MAIN
 ====================================*/
 
-int main(int argc, char* argv[]) {
-  if (argc == 2) {
-    initCompiler();
-    char* source = readFile(argv[1]);
-    compile(source);
-    freeCompiler();
-  } else if (argc == 3) {
-    scanFile(argv[2]);
+// int main(int argc, char* argv[]) {
+//   if (argc == 2) {
+//     initCompiler();
+//     char* source = readFile(argv[1]);
+//     compile(source);
+//     freeCompiler();
+//   } else if (argc == 3) {
+//     scanFile(argv[2]);
+//   } else {
+//     fprintf(stderr, "Usage: sdvu path\n");
+//     exit(64);
+//   }
+//   return 0;
+// }
+
+
+/* Main function using the CLI */
+int main (int argc, char *argv[]) {
+  struct arguments arguments;
+  FILE *outstream;
+  /* Set argument defaults */
+  arguments.outfile = NULL;
+  arguments.stringCompile = NULL;
+  arguments.stringScan = NULL;
+  arguments.stringDisassemble = NULL;
+  arguments.verbose = 0;
+  /* Argument parsing */
+  argp_parse (&argp, argc, argv, 0, 0, &arguments);
+  /* Set the output */
+  if (arguments.outfile) {
+    outstream = fopen (arguments.outfile, "w");
   } else {
-    fprintf(stderr, "Usage: sdvu path\n");
+    outstream = stdout;
+  }
+  /* Run the correct option */
+  if (arguments.stringCompile) {
+    compileFile(arguments.stringCompile, outstream);
+  } else if (arguments.stringScan) {
+    scanFile(arguments.stringScan, outstream);
+  } else if (arguments.stringDisassemble) {
+    disassembleFile(arguments.stringDisassemble, outstream);
+  } else {
     exit(64);
   }
   return 0;
